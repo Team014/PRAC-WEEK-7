5512560b4b29b28f6cd4f8896931259e5f35d0b3
igzak1 <igzak1@student.monash.edu>
2016-05-17 23:14:37 +1000

    Skeleton Code + Isaac's Initial Attempt at Location List
    My first attempt at creating local storage for a location and setting up
    the framework for our team to begin collaborating.

---
+++ Assignment 2/code/js/mainPage.js
@@ -0,0 +1,17 @@
+// Code for the main app page (locations list).
+
+// This is sample code to demonstrate navigation.
+// You need not use it for final app.
+
+function viewLocation(locationName)
+{
+   //My attempt
+    localStorage.setItem(locationName)
+    
+    
+   // Intial Data
+    //// Save the desired location to local storage
+    //localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
+    //// And load the view location page.
+    //location.href = 'viewlocation.html';
+}
